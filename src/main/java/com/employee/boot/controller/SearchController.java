4bf835067b23e22f4a2e725b0fdc404307275204
Sunita Divakaran <sundivk@gmail.com>
2026-04-11 22:35:04 +0530

    Added method for advancedSearch

--- src/main/java/com/employee/boot/controller/SearchController.java
+++ src/main/java/com/employee/boot/controller/SearchController.java
@@ -1,5 +1,7 @@
 package com.employee.boot.controller;
 
+import com.employee.boot.model.Employee;
+
 public class SearchController {
     private String searchTerm;
     public SearchController(){
@@ -10,5 +12,9 @@ public class SearchController {
     private String search(String text){
         System.out.println("search the test in db and return the value");
         return text;
+    }
+
+    private String advancedSearch(Employee emp){
+        return emp.getFirstname();
     }
 }
