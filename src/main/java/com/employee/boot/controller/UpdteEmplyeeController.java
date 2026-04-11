733e3a47a5ade05411c8b53b387fe78d1d95d747
Subhrajyoti Parida <sjparida.puri>
2026-04-11 22:35:04 +0530

    Update the Controller

--- src/main/java/com/employee/boot/controller/UpdteEmplyeeController.java
+++ src/main/java/com/employee/boot/controller/UpdteEmplyeeController.java
@@ -1,6 +1,5 @@
 package com.employee.boot.controller;
 
-
 import com.employee.boot.model.Employee;
 import com.employee.boot.repository.EmlployeeRepository;
 import org.springframework.web.bind.annotation.*;
@@ -8,14 +7,14 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/update")
 @CrossOrigin(origins = "http://localhost:4200")
-public class UpdteEmplyeeCOntroller {
+public class UpdteEmplyeeController {
 
 
     private final EmlployeeRepository empRepository;
 
-     public UpdteEmplyeeCOntroller(EmlployeeRepository empRepository){
-     this.empRepository=empRepository;
-     }
+    public UpdteEmplyeeController(EmlployeeRepository empRepository){
+        this.empRepository=empRepository;
+    }
 
     @PutMapping("/updateEmployee")
     public Employee createEmployee(@RequestBody Employee emp){
