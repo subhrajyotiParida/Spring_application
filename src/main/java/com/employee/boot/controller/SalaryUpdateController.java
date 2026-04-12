4312d5d76185af3c59a72476ace42e45c8d96fba
Sunita Divakaran <sundivk@gmail.com>
2026-04-12 12:25:30 +0530

    Added method for advancedSearch

--- src/main/java/com/employee/boot/controller/SalaryUpdateController.java
+++ src/main/java/com/employee/boot/controller/SalaryUpdateController.java
@@ -14,6 +14,11 @@ public class SalaryUpdateController
 
     }
 
+    public void deleteSalary(){
+        System.out.println("Deleted salary");
+        System.out.println("Verify salary deleted");
+    }
+
 
 
 }
